552c8008ffb04273877fc2db36ed94ff0512c44e
Yesi123456 <yg6148899@gmail.com>
2026-04-15 23:18:35 -0500

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function saludar(){
+    var nombre = document.getElementById("nombre").value;
+
+    document.getElementById("resultado").innerHTML =
+        "Hola " + nombre + ", gracias por visitar mi sitio";
+}
